80ab58ba6e8b97fa94f32e37464e89c501f19265
yiern <yiernlim96@gmail.com>
2019-04-02 13:47:50 +0800

    added address retreiever from onemap API

--- flaskapp/resource/IncidentResource.py
+++ flaskapp/resource/IncidentResource.py
@@ -3,6 +3,7 @@ from flaskapp import db
 from flaskapp.model.Incident import Incident
 from flaskapp.model.Incident import GeneralPublic
 from datetime import datetime
+import requests, json
 
 class IncidentResource(Resource):
     def get(self):
@@ -11,9 +12,6 @@ class IncidentResource(Resource):
     def post(self):
         parser = reqparse.RequestParser(bundle_errors=True)
         parser.add_argument('address', help='Address field cannot be blank', required = True)
-        parser.add_argument('postalCode', help='Postal Code field cannot be blank', required = True)
-        parser.add_argument('longtitude', help='This field cannot be blank', required = True)
-        parser.add_argument('latitude', help='This field cannot be blank', required = True)
         parser.add_argument('assignedBy', help='This field cannot be blank', required = True)
         parser.add_argument('eid', help='This field cannot be blank', required = True)
         parser.add_argument('name', help='name cannot be blank',required=True)
@@ -29,10 +27,29 @@ class IncidentResource(Resource):
         
         gp = GeneralPublic.query.filter_by(userIC=data['userIC']).first()
         gpid = gp.gpid
+    
+        
+        # get the full address lat, long and postalCode
+        address = data['address']
+        oneMap = "https://developers.onemap.sg/commonapi/search?searchVal=%s&returnGeom=Y&getAddrDetails=Y" %(address)
+        # send get request and save as response object
+        response = requests.get(oneMap)
+
+        # extract result in json format
+        result = response.json()
+        print(response.content)
+
+        latitude = result['results'][0]['LATITUDE']
+        longtitude = result['results'][0]['LONGTITUDE']
+        postalCode = result['results'][0]['POSTAL']
+        address = result['results'][0]['ADDRESS']
+
+        print(latitude, longtitude, postalCode, address)
+
 
         # Create the incident instance and add to db
-        incident =Incident(address=data['address'], postalCode=data['postalCode'], longtitude=data['longtitude'], 
-                            latitude=data['latitude'], assignedBy=data['assignedBy'], gpid=gpid, eid=data['eid'])
+        incident =Incident(address=address, postalCode=postalCode, longtitude=longtitude, 
+                            latitude=latitude, assignedBy=data['assignedBy'], gpid=gpid, eid=data['eid'])
         db.session.add(incident)
         db.session.commit()
         return data
