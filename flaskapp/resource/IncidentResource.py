c6cb0fa0e591a61ff8350475df4016e443f78e63
Wang Lu <wanglu1995.wl@gmail.com>
2019-04-04 14:20:15 +0800

    GP create incident done, updated requirement.txt with twilio

--- flaskapp/resource/IncidentResource.py
+++ flaskapp/resource/IncidentResource.py
@@ -11,6 +11,7 @@ from flask_jwt_extended import get_jwt_claims
 
 
 #Operator create incident from user call in, status = "Ongoing"
+#GP create incident set gp_create = True, has no status
 class IncidentResource(Resource): 
     def get(self):
         return {'Incident': 'world' }
@@ -25,79 +26,129 @@ class IncidentResource(Resource):
         parser.add_argument('assistance_type', action='append', help='This field cannot be blank', required=True)
         parser.add_argument('emergency_type',action='append', help='This field cannot be blank',required=True)
         parser.add_argument('relevant_agencies',action='append', help='This field cannot be blank',required=True)
+        parser.add_argument('gp_create', default=False, required=False)
         data = parser.parse_args()
 
-        # Check if a GP does not exist in database
-        if(GeneralPublic.query.filter_by(userIC=data['userIC']).first() is None):
-            gp = GeneralPublic(name=data['name'], userIC=data['userIC'], mobilePhone=data['mobilePhone'] )
-            db.session.add(gp)
-            db.session.commit()
-        
-        gp = GeneralPublic.query.filter_by(userIC=data['userIC']).first()
-        gpid = gp.gpid
-    
-        
-        # get the full address lat, long and postalCode
-        address = data['address']
-        oneMap = "https://developers.onemap.sg/commonapi/search?searchVal=%s&returnGeom=Y&getAddrDetails=Y" %(address)
-        # send get request and save as response object
-        response = requests.get(oneMap)
+        #check if is a GP create incident
+        if data['gp_create']:
+            #check if the gp exist in database
+            if(GeneralPublic.query.filter_by(userIC=data['userIC']).first() is None):
+                gp = GeneralPublic(name=data['name'], userIC=data['userIC'], mobilePhone=data['mobilePhone'] )
+                db.session.add(gp)
+                db.session.commit()
 
-        # extract result in json format
-        result = response.json()
-        print(response.content)
+            #get the gpid
+            gp = GeneralPublic.query.filter_by(userIC=data['userIC']).first()
+            gpid = gp.gpid
 
-        latitude = result['results'][0]['LATITUDE']
-        longtitude = result['results'][0]['LONGTITUDE']
-        postalCode = result['results'][0]['POSTAL']
-        address = result['results'][0]['ADDRESS']
+            # get the full address lat, long and postalCode
+            address = data['address']
+            oneMap = "https://developers.onemap.sg/commonapi/search?searchVal=%s&returnGeom=Y&getAddrDetails=Y" %(address)
+            # send get request and save as response object
+            response = requests.get(oneMap)
 
+            # extract result in json format
+            result = response.json()
+            print(response.content)
 
+            latitude = result['results'][0]['LATITUDE']
+            longtitude = result['results'][0]['LONGTITUDE']
+            postalCode = result['results'][0]['POSTAL']
+            address = result['results'][0]['ADDRESS']
 
-        # Create the incident instance and add to db
-        incident =Incident(address=address, postalCode=postalCode, longtitude=longtitude, 
+            # Create the incident instance and add to db
+            incident =Incident(address=address, postalCode=postalCode, longtitude=longtitude, 
                             latitude=latitude, gpid=gpid)
-        db.session.add(incident)
-        db.session.commit()
-
-        #update incident_request_assistanceType table
-        for x in data['assistance_type']:
-            aid = AssistanceType.query.filter_by(aid=x).first()
-            incident.assist.append(aid)
             db.session.add(incident)
+            db.session.commit()
 
-        #update incident_has_emergencyType table  
-        for y in data['emergency_type']:
-            eid = EmergencyType.query.filter_by(eid=y).first()
-            incident.emergency.append(eid)
+            #update incident_request_assistanceType table
+            for x in data['assistance_type']:
+                aid = AssistanceType.query.filter_by(aid=x).first()
+                incident.assist.append(aid)
+                db.session.add(incident)
+
+            #update incident_has_emergencyType table  
+            for y in data['emergency_type']:
+                eid = EmergencyType.query.filter_by(eid=y).first()
+                incident.emergency.append(eid)
+                db.session.add(incident)
+
+            # Store the current session data into database.
+            db.session.commit()
+            return data
+
+        else:
+            # If gp_create = False, it is operator create incident
+            # Check if a GP exist in database
+            if(GeneralPublic.query.filter_by(userIC=data['userIC']).first() is None):
+                gp = GeneralPublic(name=data['name'], userIC=data['userIC'], mobilePhone=data['mobilePhone'] )
+                db.session.add(gp)
+                db.session.commit()
+            
+            gp = GeneralPublic.query.filter_by(userIC=data['userIC']).first()
+            gpid = gp.gpid
+        
+            
+            # get the full address lat, long and postalCode
+            address = data['address']
+            oneMap = "https://developers.onemap.sg/commonapi/search?searchVal=%s&returnGeom=Y&getAddrDetails=Y" %(address)
+            # send get request and save as response object
+            response = requests.get(oneMap)
+
+            # extract result in json format
+            result = response.json()
+            print(response.content)
+
+            latitude = result['results'][0]['LATITUDE']
+            longtitude = result['results'][0]['LONGTITUDE']
+            postalCode = result['results'][0]['POSTAL']
+            address = result['results'][0]['ADDRESS']
+
+            # Create the incident instance and add to db
+            incident =Incident(address=address, postalCode=postalCode, longtitude=longtitude, 
+                                latitude=latitude, gpid=gpid)
             db.session.add(incident)
+            db.session.commit()
 
-        # Create an instance of the many to many derived table
-        # using the incident instance and agencyid instance)
-        for z in data['relevant_agencies']:
-            randomURL = generateURL()
-            agencyid = RelevantAgency.query.filter_by(agencyid=z).first()
-            assignment = IncidentAssignedToRelevantAgencies(incident=incident, relevantAgency=agencyid, link=randomURL)
-            db.session.add(assignment)
+            #update incident_request_assistanceType table
+            for x in data['assistance_type']:
+                aid = AssistanceType.query.filter_by(aid=x).first()
+                incident.assist.append(aid)
+                db.session.add(incident)
 
-        # Store the current session data into database.
-        db.session.commit()
+            #update incident_has_emergencyType table  
+            for y in data['emergency_type']:
+                eid = EmergencyType.query.filter_by(eid=y).first()
+                incident.emergency.append(eid)
+                db.session.add(incident)
 
-        #get the statusID of Ongoing from status table
-        status = Status.query.filter_by(statusName="Ongoing").first()
-        statusID = status.statusID
+            # Create an instance of the many to many derived table
+            # using the incident instance and agencyid instance)
+            for z in data['relevant_agencies']:
+                randomURL = generateURL()
+                agencyid = RelevantAgency.query.filter_by(agencyid=z).first()
+                assignment = IncidentAssignedToRelevantAgencies(incident=incident, relevantAgency=agencyid, link=randomURL)
+                db.session.add(assignment)
 
-        #get the operator id
-        operatorInfo = get_jwt_claims()
-        operatorid = operatorInfo['operatorid']
+            # Store the current session data into database.
+            db.session.commit()
 
-        #update incident_has_status table
-        status = IncidentHasStatus(statusID=statusID,incidentID=incident.incidentID,operatorid=operatorid)
-        db.session.add(status)
-        db.session.commit()
+            #get the statusID of Ongoing from status table
+            status = Status.query.filter_by(statusName="Ongoing").first()
+            statusID = status.statusID
+
+            #get the operator id
+            operatorInfo = get_jwt_claims()
+            operatorid = operatorInfo['operatorid']
+
+            #update incident_has_status table
+            status = IncidentHasStatus(statusID=statusID,incidentID=incident.incidentID,operatorid=operatorid)
+            db.session.add(status)
+            db.session.commit()
 
 
-        return data
+            return data
           
 
     def put(self):
