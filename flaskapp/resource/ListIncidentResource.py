9542eb0295b5034de57d48fdfc071339bc73162d
Tobias Macbook <tobiaslkj@gmail.com>
2019-04-15 14:44:33 +0800

    Patch for all incidents

--- flaskapp/resource/ListIncidentResource.py
+++ flaskapp/resource/ListIncidentResource.py
@@ -12,55 +12,39 @@ from pprint import pprint
 class ListIncidentResource(Resource): 
     def get(self):
         parser = reqparse.RequestParser(bundle_errors=True)
-        parser.add_argument('status', help='status field cannot be blank', required = True)
+        parser.add_argument('status',action='append', help='status field cannot be blank', required = True)
         parser.add_argument('order', help='order field cannot be blank', required = True)
         data = parser.parse_args()
         statusDictQuery = Status.query.all()
         statusDict = {}
         for s in statusDictQuery:
-            statusDict[s.statusName] = s.statusID
+            statusDict[s.statusName.lower()] = s.statusID
 
-        if data['status'] not in statusDict.keys() and data['status'].lower() != 'All'.lower():
-            return {"msg":"please input a valid status"},400
+        statusQuery = []
+        for x in data['status']:
+            if x.lower() not in statusDict.keys():
+                return {"msg":f"{x} is not a valid status"},400
+            else:
+                statusQuery.append(statusDict[x])
 
-       # Return List of incidents 
-       # Either Ongoing, Pending or All status in Asc(oldest->latest) or Desc order(latest->oldest)
-        # if(data['order'] == 'Asc'):
-        #     incidentList = Incident.query.all()
-        # elif(data['order'] =='Desc'):
-        #     incidentList = Incident.query.order_by(Incident.incidentID.desc()).all()
                 
         # call Schema class constructors
         incident_schema = IncidentSchema()
-        if data['status'].lower() == "All".lower():
-            queryObject = Incident.query
-        else:
-            queryObject = IncidentHasStatus.query.filter(IncidentHasStatus.statusID==statusDict[data['status']])
+        queryObject = Incident.query
 
         if data['order'].lower()=='Desc'.lower():
-            if data['status'].lower() == "All".lower():
-                queryObject = queryObject.order_by(Incident.incidentID.desc())
-            else:
-                queryObject = queryObject.order_by(IncidentHasStatus.incidentID.desc())
+            queryObject = queryObject.order_by(Incident.incidentID.desc())
 
         result = queryObject.all()
         
         il = []
         # Filtering and structuring
-        if data['status'].lower() != "All".lower():
-            for ihs in result:
-                if ihs.incident.statuses[len(ihs.incident.statuses)-1].statusID == statusDict[data['status']]:
-                    incidentdict = {}
-                    incidentdict['type']="Feature"
-                    incidentdict['geometry']={ "type":"Point", "coordinates":[ihs.incident.longtitude,ihs.incident.latitude] }
-                    incidentdict['properties'] = incident_schema.dump(ihs.incident)
-                    il.append(incidentdict)
-        else:
-            for ihs in result:
+        for i in result:
+            if i.statuses[len(i.statuses)-1].statusID in statusQuery:
                 incidentdict = {}
                 incidentdict['type']="Feature"
-                incidentdict['geometry']={ "type":"Point", "coordinates":[ihs.longtitude,ihs.latitude] }
-                incidentdict['properties'] = incident_schema.dump(ihs)
+                incidentdict['geometry']={ "type":"Point", "coordinates":[i.longtitude,i.latitude] }
+                incidentdict['properties'] = incident_schema.dump(i)
                 il.append(incidentdict)
 
         
