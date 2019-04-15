b9a3a48c7fdcf2bf2d2a80649506432beeb2e972
Tobias Macbook <tobiaslkj@gmail.com>
2019-04-15 14:16:13 +0800

    Added redmond part for dengue

--- flaskapp/utility/Dengue.py
+++ flaskapp/utility/Dengue.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 from pprint import pprint
+import xmltodict
+import json
 
 def getDengue():
     # Get the update geojson url
@@ -24,3 +26,21 @@ def getDengue():
         del item['properties']['Description']
 
     return json_object['features']
+
+def getDengueMeta():
+    response = requests.get("https://data.gov.sg/api/action/package_show?id=dengue-cases")
+    print(response.status_code)
+    body = response.content
+
+    den_dict = {}
+    den_dict['description'] = " ".join(
+        ((json.loads(body)['result']['description'])).split())
+
+    objects = json.loads(body)['result']['resources']
+    for object in objects:
+        name = ("".join(object['name'][17:-6].split())).lower()
+        xmlresponse = requests.get(object['url'])
+        response = json.loads(json.dumps(xmltodict.parse(xmlresponse.content)))
+        den_dict[name] = response['kml']['Document']['Folder']['Placemark'][0]['ExtendedData']['SchemaData']['SimpleData'][1]['#text'][-1]
+    
+    return den_dict
